34791c5be0d16b26cdd050f1a85a49d818dbcb8e
paulacarrio <carriocordo.paula@gmail.com>
2018-01-21 22:53:48 +0100

    ods-yml tools

---
+++ tools/ods_to_yml.py
@@ -0,0 +1,52 @@
+# Read mappings from ods into yaml file
+
+from pyexcel_ods import get_data
+data = get_data("manual_mapping.ods")
+table = data['Sheet1']
+
+import yaml
+for i in range(0, len(table)):
+
+    try:
+        diagnosis = table[i][0]
+        icdom_c = 'icdom:'+(table[i][1]).replace("/","_")
+        icdom = table[i][2]
+        icdot_c = 'icdot:'+(table[i][3]).replace(".","_")
+        icdot = table[i][4]
+        seer = 'seer:'+ str(table[i][5])
+        ncit = table[i][6]
+        ncit_c = 'ncit:'+table[i][7]
+
+        mapping = {
+            'input':[
+                {'term_id' : icdom_c,
+                'term' : icdom
+                },
+                {
+                    'term_id' : icdot_c,
+                    'term' : icdot
+                }
+            ],
+
+            'equivalents':[
+                {
+                    'term_id' : ncit_c,
+                    'term' : ncit
+                },
+                {
+                    'term_id' : seer
+                }
+            ],
+
+            'examples':[
+            {
+                'term' : diagnosis
+            }
+            ]
+        }
+
+        with open((icdom_c).replace("/","_").replace(":","_") +','+ ((icdot_c).replace("/","_").replace(":","_")) +'.yml','w') as outfile:
+            yaml.safe_dump(mapping, outfile, default_flow_style=False)
+
+    except:
+        pass
---
+++ tools/yml_to_ods.py
@@ -0,0 +1,32 @@
+# Script to read yml files and write an ods spreadsheet for mapping codes
+
+import yaml
+import os
+import collections
+from pyexcel_ods import save_data
+
+mapp = []
+for filename in os.listdir('/'): # Specify directory
+    if filename.endswith("yml"):
+        with open(filename, "r") as stream:
+            data = yaml.load(stream)
+            #print data
+            try:
+                diagnosis = [data['examples'][0]['term']]
+                icdom_c = [data['input'][0]['term_id'].replace('icdom:','').replace("_","/")]
+                icdom = [data['input'][0]['term']]
+                icdot_c = [data['input'][1]['term_id'].replace('icdot:','').replace("_",".")]
+                icdot = [data['input'][1]['term']]
+                seer = [data['equivalents'][1]['term_id'].replace('seer:','')]
+                ncit = [data['equivalents'][0]['term']]
+                ncit_c = [data['equivalents'][0]['term_id'].replace("ncit:","")]
+                mapp.append(diagnosis+icdom_c+icdom+icdot_c+icdot+seer+ncit+ncit_c)
+            except:
+                pass
+
+#print mapp
+
+mapping = collections.OrderedDict()
+
+sheetx = {"codes":mapp}
+save_data("updated_mappings.ods",sheetx)
